4b53dfdb930cd911cb032f9c0dbce96d6d3fc278
GingerNinjaLab <74956755+GingerNinjaLab@users.noreply.github.com>
2021-06-03 09:52:14 +0100

    Initial commit

---
+++ LockScreen/LockScreen/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+
+namespace LockScreen
+{
+    class Program
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool LockWorkStation();
+
+        static void Main(string[] args)
+        {
+            if (!LockWorkStation())
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+        }
+    }
+}
